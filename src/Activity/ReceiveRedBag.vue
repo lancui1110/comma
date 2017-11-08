97b83d1a56d014ab25f871288cd30fd38ce9c934
LanCui <lancui@LanCuideMacBook-Pro.local>
2017-11-08 14:33:24 +0800

    add 推荐

--- src/Activity/ReceiveRedBag.vue
+++ src/Activity/ReceiveRedBag.vue
@@ -12,9 +12,9 @@
         <div class="form-item"><input type="text" placeholder="输入手机号" class="input-item"></div>
         <div class="form-item code-input-panel">
           <input type="text" placeholder="输入验证码" class="input-item">
-          <span class="btn">获取</span>
+          <span class="yellow-btn">获取</span>
         </div>
-        <div class="form-item btn">立即领取</div>
+        <div class="form-item yellow-btn">立即领取</div>
       </div>
 
       <!-- 已领过优惠券 -->
@@ -53,27 +53,19 @@
 
       <div class="content-block">
         <div class="title">活动规则</div>
-        <p>1、每张优惠券同一设备、同一ID、同一用户每天仅限领取一次；</p>
-        <p>2、每个订单仅限使用一张优惠券；</p>
-        <p>3、优惠券金额不能兑换现金</p>
+        <p v-for="(item, i) in Content.activityRule" :key="i">{{item}}</p>
       </div>
       <div class="content-block">
         <div class="title">产品介绍</div>
-        <p>逗号mini便利，适合办公室的无人值守小超市，20步距离的能量补给站，解锁零售方式，90%的价钱解决100%的需求。</p>
+        <p v-for="(item, i) in Content.productIntroduce" :key="i">{{item}}</p>
       </div>
 
       <div class="circles">
-        <img :src="require('../assets/activity/circle.png')" alt="" />
-        <img :src="require('../assets/activity/circle.png')" alt="" />
-        <img :src="require('../assets/activity/circle.png')" alt="" />
-        <img :src="require('../assets/activity/circle.png')" alt="" />
+        <img v-for="(item, i) in Content.circlePics" :key="i" :src="require(`../assets/activity/${item}`)" alt="" />
       </div>
 
       <div class="content-box">
-        <p>精选品质，国外网红商品，1网打尽；</p>
-        <p>不交押金、丢货免赔、免费配送；</p>
-        <p>早餐、零食、宵夜、1站解决；</p>
-        <p>产品每日配送，每周上新，1马当先</p>
+        <p v-for="(item, i) in Content.content" :key="i">{{item}}</p>
       </div>
     </div>
     <div class="bottom">
@@ -84,6 +76,7 @@
 
 <script>
 // import { mapGetters } from 'vuex'
+import Content from './content'
 
 export default {
   name: 'ReceiveRedBag',
@@ -91,6 +84,7 @@ export default {
   },
   data () {
     return {
+      Content,
       state: 2, // 是否已登录
       coupon: {
         name: '新人礼包1',
@@ -107,10 +101,8 @@ export default {
 </script>
 
 <style lang="less">
-  @import "../global/style/theme.less";
+  @import "./activity.less";
 
-  @red:#E93C4D;
-  
   .recieve-reabag-panel {
     position: relative;
     width: 100%;
@@ -146,7 +138,7 @@ export default {
       .form-item {
         margin-bottom: 30/@R;
       }
-      .input-item, .btn {
+      .input-item {
         width: 497/@R;
         height: 101/@R;
         text-align: center;
@@ -156,13 +148,6 @@ export default {
         font-size: 36/@R;
         padding: 33/@R 20/@R;
       } 
-      .btn {
-        width: 497/@R;
-        background: #F9DC57;
-        font-size: 42/@R;
-        color: #A91414;
-        line-height: 101/@R;
-      }
       .code-input-panel {
         width: 497/@R;
         display: flex;
@@ -170,60 +155,10 @@ export default {
         .input-item {
           width: 350/@R;
         }
-        .btn {
+        .yellow-btn {
           width: 120/@R;
           font-size: 32/@R;
         }
-      }
-    }
-    .content-block {
-      position: relative;
-      border-top: 1px solid #fff;
-      font-size: 28/@R;
-      margin: 63/@R 40/@R 0 40/@R;
-      padding: 56/@R 14/@R 0 14/@R;
-      .title {
-        position: absolute;
-        top: -30/@R;
-        left: 196/@R;
-        width: 287/@R;
-        text-align: center;
-        background: @red;
-        color: #fff;
-      }
-      p {
-        line-height: 56/@R;
-      }
-    }
-    .circles {
-      margin: 34/@R 30/@R;
-      img {
-        width: 160/@R;
-        height: 160/@R;
-        margin-right: 5/@R;
-        &:nth-child(4) {
-          margin-right: 0;
-        }
-      }
-    }
-    .content-box {
-      margin: 0 62/@R;
-      border: 1px solid #F1B06D;
-      border-radius: 29/@R;
-      padding: 40/@R 0 220/@R 90/@R;
-      p {
-        line-height: 56/@R;
-      }
-    }
-    .bottom {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      font-size: 0;
-      line-height: 0;
-      .img {
-        width: 100%;
       }
     }
 
@@ -240,6 +175,7 @@ export default {
       }
       .coupons {
         position: relative;
+        margin-bottom: 20/@R;
       }
       .left {
         display: inline-block;
