cc3ab856682c20911bb5fc4ff78fb9515e78ac30
LanCui <lancui@LanCuideMacBook-Pro.local>
2018-01-16 17:47:04 +0800

    update

--- src/global/wx/wxsa.js
+++ src/global/wx/wxsa.js
@@ -13,7 +13,7 @@ const weixin = {
     }).then(res => {
       if (res && res.code === 1) {
         wx.config({
-          debug: true,
+          debug: false,
           appId: res.data.appId,
           timestamp: res.data.timestamp,
           nonceStr: res.data.nonceStr,
