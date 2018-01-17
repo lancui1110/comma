392f97f6005ba1c4f72ffd8b6f1217c12a0e53b1
wjs <weijinshi@squarevalleytech.com>
2018-01-17 11:51:11 +0800

    #156 购物车加号没有效果了～这里的效果同一期

--- src/Home/components/ProductItem.vue
+++ src/Home/components/ProductItem.vue
@@ -54,11 +54,45 @@ export default {
     }
   },
   methods: {
-    addToCart () {
+    addToCart (e) {
       if (this.data.status === '售罄') {
         MessageBox('商品可能售罄了', '请您在货架上确认后再购买！')
       }
-      this.$store.dispatch('home/addToCart', this.data)
+      // this.$store.dispatch('home/addToCart', this.data)
+
+      const initX = e.clientX - 10
+      const initY = e.clientY - 10
+      const targetX = 30
+      const targetY = screen.height - 50
+      let curvature = 0.9
+      let speed = 6
+      if (initX < screen.width / 2) {
+        curvature = 1.6
+        speed = 3
+      } else if (initX > screen.width / 3 * 2) {
+        curvature = 0.6
+        speed = 10
+      }
+
+      const ball = document.createElement('div')
+      ball.appendChild(document.createTextNode('1'))
+      ball.setAttribute('class', 'ball')
+      ball.style.top = initY + 'px'
+      ball.style.left = initX + 'px'
+      document.body.insertAdjacentElement('afterbegin', ball)
+      const p = new iwjw.Parabola({
+        el: ball,
+        direction: 'down',
+        curvature: curvature,
+        speed: speed,
+        origin: { x: initX, y: initY },
+        target: { x: targetX, y: targetY },
+        onMotionDone: () => {
+          ball.remove()
+          this.$store.dispatch('home/addToCart', this.data)
+        }
+      })
+      p.start()
     },
     removeFromCart () {
       this.$store.dispatch('home/removeFromCart', this.data)
@@ -151,4 +185,15 @@ export default {
       }
     }
   }
+  .ball {
+    position: fixed;
+    width: 20px;
+    height: 20px;
+    text-align: center;
+    border-radius: 50%;
+    background-color: @primary;
+    color: #fff;
+    z-index: 999;
+  }
+
 </style>
